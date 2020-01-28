2f2c03cafebb05f79cfe08a352b09d95394bc841
fujihiraryo <fujihiraryo@team-lab.com>
2020-01-29 01:03:28 +0900

    log

--- classifier/curve.py
+++ classifier/curve.py
@@ -1,25 +1,28 @@
+# pytorch
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
+# my modules
 import model
 import dataloader
 import settings
 
+# standard packages
 import matplotlib.pyplot as plt
 from datetime import datetime as dt
+import time
 
 cnn = model.CNN()
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(cnn.parameters(),
-                      lr=settings.lr,
-                      momentum=settings.momentum)
+optimizer = optim.SGD(cnn.parameters(), lr=settings.lr, momentum=settings.momentum)
 trainloader = dataloader.trainloader
 testloader = dataloader.testloader
 
 train_loss = []
 test_loss = []
 
+start = time.time()
 for epoch in range(settings.epochs):
     for i, data in enumerate(trainloader):
         inputs, labels = data
@@ -35,9 +38,14 @@ for epoch in range(settings.epochs):
         loss = criterion(outputs, labels)
         test_loss.append(loss.item())
 
+    text = "epoch:{:3d} train:{:3.3f} test:{:3.3f} time:{:5.3f}".format(
+        epoch + 1, train_loss[-1], test_loss[-1], time.time() - start
+    )
+    print(text)
+
 K = len(train_loss)
-plt.plot(range(K), train_loss, label='train')
-plt.plot(range(K), test_loss, label='test')
+plt.plot(range(K), train_loss, label="train")
+plt.plot(range(K), test_loss, label="test")
 plt.legend()
 now = dt.now().strftime("%Y%m%d%H%M%S")
-plt.savefig('result/'+now+'.png')
+plt.savefig("result/{}.png".format(now))
--- classifier/settings.py
+++ classifier/settings.py
@@ -2,6 +2,6 @@ mean = (0.5, 0.5, 0.5)
 sigma = (0.5, 0.5, 0.5)
 batch_size = 2000
 num_workers = 5
-epochs = 10
+epochs = 5
 lr = 0.1
 momentum = 0
