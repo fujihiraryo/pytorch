0e5cba6c7b7926567c7cb850fa2721afcde50a9a
fujihiraryo <fujihiraryo@team-lab.com>
2020-01-29 22:44:26 +0900

    change dir name

---
+++ cifar_classifier/sample.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision
+import dataloader
+
+trainloader = dataloader.trainloader
+testloader = dataloader.testloader
+images, labels = iter(trainloader).next()
+images, labels = images[:25], labels[:25]
+grid_image = torchvision.utils.make_grid(images, nrow=5, padding=1)
+grid_image = grid_image.numpy()
+grid_image = grid_image/2+0.5
+grid_image = np.transpose(grid_image, (1, 2, 0))
+plt.imsave('image/sample.png', grid_image)
---
+++ cifar_classifier/setup.py
@@ -0,0 +1,12 @@
+import torchvision
+
+torchvision.datasets.CIFAR10(
+    root='./data',
+    train=True,
+    download=True
+)
+torchvision.datasets.CIFAR10(
+    root='./data',
+    train=False,
+    download=True
+)
